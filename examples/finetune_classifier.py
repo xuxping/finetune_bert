0217dfd898bd8c7c80ded676dac3b34d82139309
xuxping <xuxp2018@163.com>
2019-12-13 19:27:59 +0800

    update train

--- examples/finetune_classifier.py
+++ examples/finetune_classifier.py
@@ -12,10 +12,11 @@ from tensorflow.python.keras.callbacks import ModelCheckpoint, TensorBoard
 from finetune.modeling_bert import BertForSequenceClassification
 from tensorflow.python.keras.models import load_model
 from sklearn.metrics import classification_report, confusion_matrix
-from finetune.optimizers import AdamWarmup
 from finetune.tokenization_bert import BertTokenizer
 from finetune.dataset import ChnSentiCorpDataset
 import time
+
+from datetime import datetime
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -28,39 +29,52 @@ def set_random():
     tf.set_random_seed(42)
 
 
+TASK_NAMES = {
+    'chnsenticorp': ChnSentiCorpDataset
+}
+
+
 def train(opts):
     tokenizer = BertTokenizer.from_pretrained(opts.pretrained_path)
-    dataset = ChnSentiCorpDataset(opts.data_dir, tokenizer, opts.max_seq_len)
 
+    # get dataset
+    dataset = TASK_NAMES[opts.task_name](opts.data_dir, tokenizer, opts.max_seq_len)
     X_train, y_train = dataset.get_train_datasets()
     X_dev, y_dev = dataset.get_dev_datasets()
-    decay_steps = (len(y_train) // opts.batch_size) * opts.epochs
-    print("decay_steps==", decay_steps)
-    optimizer = AdamWarmup(decay_steps=decay_steps,
-                           warmup_steps=0,
-                           lr=opts.lr,
-                           weight_decay=0.01,
-                           clipnorm=1.0)
+    if not opts.use_token_type:
+        X_train = X_train[0]
+        X_dev = X_dev[0]
+    # build model
+    optimizer = tf.keras.optimizers.Adam(lr=opts.lr, epsilon=1e-08)
 
-    model = BertForSequenceClassification().build(opts)
+    bert = BertForSequenceClassification.from_pretrained(
+        pretrained_path=opts.pretrained_path,
+        trainable=True,
+        training=False,
+        max_seq_len=opts.max_seq_len,
+        num_labels=len(dataset.get_labels())
+    )
+    model = bert.model
     model.compile(
         optimizer=optimizer,
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
-    print(model.summary())
+    model.summary()
 
     # callbacks: save model
     filepath = os.path.join(opts.save_dir, "{epoch:02d}-{val_acc:.4f}.hdf5")
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=False, mode='max')
 
     # callbacks: tensorboard
-    tensorboard = TensorBoard(log_dir=opts.log_dir)
+    tensorboard_dir = os.path.join(opts.log_dir, datetime.now().strftime("%Y%m%d-%H%M"))
+    tensorboard = TensorBoard(log_dir=tensorboard_dir)
 
     model.fit(X_train, y_train,
               batch_size=opts.batch_size,
               epochs=opts.epochs,
               validation_data=(X_dev, y_dev),
+              shuffle=True,
               callbacks=[checkpoint, tensorboard])
 
     X_test, y_test = dataset.get_test_datasets()
@@ -69,28 +83,18 @@ def train(opts):
     print('test accuracy:', acc)
 
 
-def get_predict(x, thresold=0.5):
-    y_pred = []
-    for pred in x:
-        if pred[0] > thresold:
-            y_pred.append(0)
-        else:
-            y_pred.append(1)
-    return y_pred
-
-
 def test(opts):
     tokenizer = BertTokenizer.from_pretrained(opts.pretrained_path)
-    dataset = ChnSentiCorpDataset(opts.data_dir, tokenizer, opts.max_seq_len)
+    dataset = TASK_NAMES[opts.task_name](opts.data_dir, tokenizer, opts.max_seq_len)
     X_test, y_test = dataset.get_test_datasets()
-
+    if not opts.use_token_type:
+        X_test = X_test[0]
     # use get custiom_object to load model
     model = load_model(opts.save_dir)
 
     start_time = time.time()
-
     y_pred = model.predict(X_test, batch_size=opts.batch_size)
-    y_pred = np.argmax(y_pred)
+    y_pred = np.argmax(y_pred, axis=1)
 
     end_time = time.time()
     qps = float(len(y_test) + 1) / float(end_time - start_time + 1)
@@ -101,34 +105,31 @@ def test(opts):
 
     if opts.write_test_result:
         with codecs.open(opts.write_test_result, 'w', encoding='utf-8') as file_writer:
-            for pred, label in zip(y_pred, y_test):
-                file_writer.write('{}\t{}\n'.format(pred, label[0]))
+            for pred, label, token_id in zip(y_pred, y_test, X_test[0]):
+                tokens = tokenizer.convert_ids_to_tokens(token_id)
+                file_writer.write('{}\t{}\t{}\n'.format(pred, label, ''.join(tokens)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
     parser.add_argument('--train', action='store_true', help='train mode')
     parser.add_argument('--test', action='store_true', help='test mode')
-    parser.add_argument('--test_threshold', action='store_true', help='test mode')
+
+    parser.add_argument('--task_name', type=str, default='chnsenticorp', choices=TASK_NAMES.keys())
     parser.add_argument('--data_dir', type=str, default='../datasets/chnsenticorp')
-
     parser.add_argument('--pretrained_path', type=str, default=None, help='bert pretrained path')
-
-    parser.add_argument('--max_seq_len', type=int, default=256, help='max seq len')
-    parser.add_argument('--lr', type=float, default=5e-5, help='learning rate')
-    parser.add_argument('--hidden_dropout_prob', type=float, default=0.2, help='dropout rate')
-    parser.add_argument('--batch_size', type=int, default=24, help='max seq len')
-    parser.add_argument('--num_labels', type=int, default=2, help='num labels')
-    parser.add_argument('--initializer_range', type=float, default=0.02, help='initializer range')
-    parser.add_argument('--epochs', type=int, default=10, help='train epochs')
-    parser.add_argument('--save_dir', type=str, default=None,
-                        help='model save dir')
-    parser.add_argument('--log_dir', type=str, default='./logs', help='run log dir')
+    parser.add_argument('--max_seq_len', type=int, default=128, help='max seq len')
+    parser.add_argument('--lr', type=float, default=3e-5, help='learning rate')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--use_token_type', type=int, default=0, help='use_token_type')
+    parser.add_argument('--epochs', type=int, default=4, help='train epochs')
+    parser.add_argument('--save_dir', type=str, default=None, help='model save dir')
+    parser.add_argument('--log_dir', type=str, default='./logs', help='tensorboard log dir')
 
     parser.add_argument('--write_test_result', type=str, default=None, help='file to write test result')
 
     opts = parser.parse_args()
+    assert opts.task_name in TASK_NAMES
 
     set_random()
     if opts.train:
